7fca0c600a26881d2cfc9446eb9d5a64c2b7a621
sanlogo <sanlogo@icloud.com>
2026-09-06 22:16:49 +0200

    Delete Shadow Samurei directory

--- Shadow Samurei/kimi3_ai_test_fixed 2/dev_tools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Entwicklerwerkzeuge: Metriken, Benchmarks, Schicht-Training und Bewertungen.
-
-Die Module dieses Pakets werden bewusst erst bei Bedarf importiert, damit die
-Oberfläche auch ohne installiertes PyTorch startet.
-"""
-from dev_tools.metrics_tracker import MetricEntry, MetricsTracker, hole_verfolgung
-
-__all__ = ["MetricEntry", "MetricsTracker", "hole_verfolgung"]
